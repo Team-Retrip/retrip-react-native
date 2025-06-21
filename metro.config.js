5100b86dae0368f6e4cca6ee93f11d9eac92e87b
ganghee <idiosauri@naver.com>
2025-06-21 20:52:09 +0900

    프로젝트 생성

--- metro.config.js
+++ metro.config.js
@@ -8,4 +8,4 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+exports = mergeConfig(getDefaultConfig(__dirname), config);
